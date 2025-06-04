256e9c746d57e57baa6d6ff4acda251059053117
Jonas Hölscher <jonasho@mail.uni-paderborn.de>
2025-06-04 17:25:07 +0200

    Fix bug in importing

--- plutho/__init__.py
+++ plutho/__init__.py
@@ -5,7 +5,7 @@ from .io import parse_charge_hist_file, parse_displacement_hist_file, \
     create_vector_field_as_csv, create_scalar_field_as_csv
 from .postprocessing import calculate_impedance, \
     calculate_electrical_input_energy, calculate_stored_thermal_energy
-from .materials import pic255, MaterialManager
+from .materials import MaterialManager
 from .single_sim import SingleSimulation, FieldType
 from .mesh import Mesh
 from .coupled_sim import CoupledThermoPiezoThermoSim, CoupledFreqPiezoTherm
