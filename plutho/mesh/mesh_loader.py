72d0824791720297cacf81bca68b1b6fe63a4850
Jonas Hölscher <hoelscher@emt.uni-paderborn.de>
2025-06-27 15:50:17 +0200

    Fix gmsh v2 parser

--- plutho/mesh/mesh_loader.py
+++ plutho/mesh/mesh_loader.py
@@ -37,6 +37,9 @@ class MeshFormat:
 class Element:
     node_indices: List[int]
     type: int
+    # The first tag is the physical entity, the second one is the elementary
+    # model entity and if it exists, the third one is the number of mesh
+    # partitions to which the element belongs
     tags: List[int]
 
 
@@ -203,7 +206,7 @@ class GmshData:
             if dimension != element.type:
                 continue
 
-            if tag in element.tags:
+            if tag == element.tags[0]:  # Since this is the pg tag
                 found_elements.append(element.node_indices)
 
         if not found_elements:
@@ -213,6 +216,31 @@ class GmshData:
             )
 
         return np.array(found_elements)
+
+    def get_triangle_elements_by_physical_group(
+        self,
+        physical_group_name: str
+    ) -> npt.NDArray:
+        elements_pg = self.get_elements_by_physical_group(physical_group_name)
+        _, elements = self.get_mesh_nodes_and_elements()
+
+        # Find triangle elements, which share the same nodes
+        triangle_elements = []
+        for element_pg in elements_pg:
+            for element in elements:
+                if len(element) != 3:
+                    continue
+
+                found = True
+                for node_index in element_pg:
+                    if node_index not in element:
+                        found = False
+                        break
+
+                if found:
+                    triangle_elements.append(element)
+
+        return np.array(triangle_elements)
 
 
 class MeshParser:
@@ -426,23 +454,40 @@ if __name__ == "__main__":
     pg_elements_v2 = {}
     for pg in physical_groups:
         pg_nodes_v2[pg] = mesh_v2.get_nodes_by_physical_group(pg)
-        pg_elements_v2[pg] = mesh_v2.get_elements_by_physical_group(pg)
+        pg_elements_v2[pg] = mesh_v2.get_triangle_elements_by_physical_group(
+            pg
+        )
 
     if not np.array_equal(nodes_v2, nodes_v4):
         print("Nodes not equal")
     if not np.array_equal(elements_v2, elements_v4):
         print("Elements not equal")
 
+    def check_same_contents(l1, l2):
+        for item in l1:
+            if item not in l2:
+                return False
+
+        for item in l2:
+            if item not in l1:
+                return False
+
+        return True
+
+    # The physical group lists could be ordered differently
     for pg in physical_groups:
         nodes_v2 = pg_nodes_v2[pg]
         nodes_v4 = pg_nodes_v4[pg]
         elements_v2 = pg_elements_v2[pg]
         elements_v4 = pg_elements_v4[pg]
 
-        print("Elements v2", elements_v2)
-        print("Elements v4", elements_v4)
-
-        if not np.array_equal(nodes_v2, nodes_v4):
+        if not check_same_contents(nodes_v2, nodes_v4):
             print(f"({pg}) Physical group nodes not equal")
-        if not np.array_equal(elements_v2, elements_v4):
+            print("Nodes v2", nodes_v2)
+            print("Nodes v4", nodes_v4)
+        if not check_same_contents(elements_v2, elements_v4):
             print(f"({pg}) Physical group elements not equal")
+            print("Elements v2", elements_v2)
+            print("Elements v4", elements_v4)
+            e = mesh_v2.get_elements_by_physical_group(pg)
+            print(f"{pg} Elements:", e)
