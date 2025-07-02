c22337d8283c01680f763b60c9ff7d656c41082f
Jonas Hölscher <hoelscher@emt.uni-paderborn.de>
2025-07-02 10:28:41 +0200

    Update test data due to gmsh bug

--- tests/test_simulations.py
+++ tests/test_simulations.py
@@ -453,15 +453,9 @@ def generate_data():
     plutho is correct."""
     dir = "tests/data"
 
-    # Check if mesh exists
-    mesh_path = os.path.join(dir, "default_mesh.msh")
-    if not os.path.exists(mesh_path):
-        mesh = plutho.Mesh(mesh_path)
-        mesh.generate_rectangular_mesh()
-
     test_piezo_time(dir, test=False)
-    #test_piezo_freq(dir, test=False)
-    #test_thermo_piezo_time(dir, test=False)
+    test_piezo_freq(dir, test=False)
+    test_thermo_piezo_time(dir, test=False)
 
 
 if __name__ == "__main__":
